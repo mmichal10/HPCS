21da12494643d30ee2ab7ba2138f0bb74eb7a5f3
Michal Mielewczyk <michal.mielewczyk10@gmail.com>
2019-03-20 22:20:31 +0100

    Splited main into smaller functions.

--- lab2/code.c
+++ lab2/code.c
@@ -16,7 +16,7 @@
 #define RESULT 1
 #define FINISH 2
 
-#define X_DATA_SIZE 10
+#define X_DATA_SIZE 700
 #define Y_DATA_SIZE X_DATA_SIZE
 #define DATA_SIZE (X_DATA_SIZE * Y_DATA_SIZE)
 
@@ -36,6 +36,37 @@ static void prepare_rand_data(double data[][Y_DATA_SIZE]) {
 				data[i][j] = rand() % DATA_SIZE;
 			}
 		}
+}
+
+static void prepare_chunk_from_data(int *x, int *y, double *dst,
+		double data[][Y_DATA_SIZE])
+{
+	int j = 0;
+	while (j < CHUNK_SIZE &&
+			*x < X_DATA_SIZE && *y < Y_DATA_SIZE)
+	{
+		dst[j++] = data[*x][*y];
+		if (++(*y) >= Y_DATA_SIZE) {
+			*y = 0;
+			(*x)++;
+		}
+	}
+}
+
+static void retrive_data_from_chunk(int offset, double *src,
+		double dst[][Y_DATA_SIZE])
+{
+	int dst_x = offset / Y_DATA_SIZE;
+	int dst_y = offset % Y_DATA_SIZE;
+	int i;
+
+	for (i = 0; i < CHUNK_SIZE; i++) {
+		dst[dst_x][dst_y] = src[i];
+		if (++dst_y >= Y_DATA_SIZE) {
+			dst_y = 0;
+			dst_x++;
+		}
+	}
 }
 
 
@@ -64,6 +95,7 @@ int main (int argc, char **argv)
 		return -1;
 	}
 
+	fflush(stdout);
 	if (myrank == 0)
 	{
 		double rand_arr[X_DATA_SIZE][Y_DATA_SIZE];
@@ -74,108 +106,58 @@ int main (int argc, char **argv)
 
 		prepare_rand_data(rand_arr);
 
-		/*for (i = 0; i < X_DATA_SIZE; i++) {
-			for (j = 0; j < Y_DATA_SIZE; j++) {
-				printf(" %f ", rand_arr[i][j]);
-			}
-			printf("\n");
-			fflush (stdout);
-		}*/
-
 		for (proc_id = 1; proc_id < proccount; proc_id++) {
-			j = 0;
 			offset = x_pos * Y_DATA_SIZE + y_pos;
-			while (j < CHUNK_SIZE &&
-					x_pos < X_DATA_SIZE && y_pos < Y_DATA_SIZE)
-			{
-				tmp_arr[j++] = rand_arr[x_pos][y_pos];
-				if (++y_pos >= Y_DATA_SIZE) {
-					y_pos = 0;
-					x_pos++;
-				}
-			}
+			prepare_chunk_from_data(&x_pos, &y_pos, tmp_arr, rand_arr);
+
 			MPI_Send(&offset, 1, MPI_UNSIGNED, proc_id, OFFSET, MPI_COMM_WORLD);
 			MPI_Send(tmp_arr, CHUNK_SIZE, MPI_DOUBLE, proc_id, DATA,
 					MPI_COMM_WORLD);
 		}
 
 		while (x_pos < X_DATA_SIZE) {
-			int dst_proc;
 			MPI_Recv(&offset, 1, MPI_UNSIGNED, MPI_ANY_SOURCE, OFFSET,
 					MPI_COMM_WORLD, &status);
-			dst_proc = status.MPI_SOURCE;
-			MPI_Recv(&tmp_arr, CHUNK_SIZE, MPI_DOUBLE, dst_proc, DATA,
+			proc_id = status.MPI_SOURCE;
+			MPI_Recv(&tmp_arr, CHUNK_SIZE, MPI_DOUBLE, proc_id, DATA,
 					MPI_COMM_WORLD, &status);
-			
 
-			dst_x = offset / Y_DATA_SIZE;
-			dst_y = offset % Y_DATA_SIZE;
+			retrive_data_from_chunk(offset, tmp_arr, result_arr);
 
-			for (i = 0; i < CHUNK_SIZE; i++) {
-				result_arr[dst_x][dst_y] = tmp_arr[i];
-				if (++dst_y >= Y_DATA_SIZE) {
-					dst_y = 0;
-					dst_x++;
-				}
-			}
-
-			j = 0;
 			offset = x_pos * Y_DATA_SIZE + y_pos;
-			while (j < CHUNK_SIZE &&
-					x_pos < X_DATA_SIZE && y_pos < Y_DATA_SIZE)
-			{
-				tmp_arr[j++] = rand_arr[x_pos][y_pos];
-				if (++y_pos >= Y_DATA_SIZE) {
-					y_pos = 0;
-					x_pos++;
-				}
-			}
-			MPI_Send(&offset, 1, MPI_UNSIGNED, dst_proc, OFFSET,
+			prepare_chunk_from_data(&x_pos, &y_pos, tmp_arr, rand_arr);
+			MPI_Send(&offset, 1, MPI_UNSIGNED, proc_id, OFFSET,
 					MPI_COMM_WORLD);
-			MPI_Send(tmp_arr, CHUNK_SIZE, MPI_DOUBLE, dst_proc, DATA,
+			MPI_Send(tmp_arr, CHUNK_SIZE, MPI_DOUBLE, proc_id, DATA,
 					MPI_COMM_WORLD);
 		}
 
 		for (i = 1; i < proccount; i++) {
 			MPI_Recv(&offset, 1, MPI_UNSIGNED, MPI_ANY_SOURCE, OFFSET,
 					MPI_COMM_WORLD, &status);
-			MPI_Recv(&tmp_arr, CHUNK_SIZE, MPI_DOUBLE, status.MPI_SOURCE, DATA,
+			proc_id = status.MPI_SOURCE;
+			MPI_Recv(&tmp_arr, CHUNK_SIZE, MPI_DOUBLE, proc_id, DATA,
 					MPI_COMM_WORLD, &status);
 
-			dst_x = offset / Y_DATA_SIZE;
-			dst_y = offset % Y_DATA_SIZE;
-
-			for (j = 0; j < CHUNK_SIZE; j++) {
-				result_arr[dst_x][dst_y] = tmp_arr[j];
-				if (++dst_y >= Y_DATA_SIZE) {
-					dst_y = 0;
-					dst_x++;
-				}
-			}
+			retrive_data_from_chunk(offset, tmp_arr, result_arr);
 		}
 
 
 		for (i = 1; i < proccount; i++)
-			MPI_Send (NULL, 0, MPI_DOUBLE, i, FINISH, MPI_COMM_WORLD);
+			MPI_Send(NULL, 0, MPI_DOUBLE, i, FINISH, MPI_COMM_WORLD);
 
-			printf("\n");
-			printf("\n");
-			printf("\n");
 		for (i = 0; i < X_DATA_SIZE; i++) {
 			for (j = 0; j < Y_DATA_SIZE; j++) {
 				printf(" %f ", result_arr[i][j]);
 			}
 			printf("\n");
-			fflush (stdout);
 		}
+		fflush (stdout);
 
-
-		// now display the result
-		printf ("\nHi, I am process 0, the result is\n");
 	} else {
 		// slave
 		do {
-			MPI_Probe (0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			MPI_Probe(0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 			if (status.MPI_TAG == OFFSET) {
 				MPI_Recv(&offset, 1, MPI_UNSIGNED, 0, OFFSET,
 						MPI_COMM_WORLD, &status);
@@ -189,7 +171,7 @@ int main (int argc, char **argv)
 			}
 		} while (status.MPI_TAG != FINISH);
 	}
-	// Shut down MPI
+
 	MPI_Finalize ();
 
 	return 0;
